4b1ee29d028ba8ce3e3ae7811cdcb4b842f87184
Kederly <maksnn@inbox.ru>
2019-10-15 14:55:54 +0300

    Тестовый коммит

---
+++ OrkShop.py
@@ -0,0 +1,84 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import os
+
+os.remove('OrkShop.csv')
+
+start_url = 'https://goodork.ru/categories/astra-militarum?page=1'
+base = 'https://goodork.ru/categories/astra-militarum?page='
+name = 'Astra'
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+def get_total_pages(html):
+    soup = BeautifulSoup(html, 'html5lib')
+    pages = soup.find('div', class_='pagenumberer')
+    if pages is None:
+        total_pages = '1'
+    else:
+        pages = \
+        soup.find('div', class_='pagenumberer').find_all('a', class_='pagenumberer-item pagenumberer-item-link')[-1].get('href')
+        total_pages = pages.split('=')[1]
+
+    return int(total_pages)
+
+def write_csv(data):
+    with open('OrkShop.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow((data['race'],
+                         data['title'],
+                         data['price'],
+                         data['status'],
+                         data['url']))
+
+def get_page_data(html):
+    soup = BeautifulSoup(html, 'html5lib')
+
+    ads = soup.find('div', class_='row products-view products-view-tile productview-wow').find_all('div', class_='products-view-block')
+
+    for ad in ads:
+    #name, price, status, url
+        try:
+            title = ad.find('div', class_='products-view-name products-view-name-default').find('a').get('title')
+        except:
+            title = ''
+        try:
+            price = ad.find('div', class_='price-new cs-t-1').find('div', class_='price-number').text.strip() + ' руб.'
+        except:
+            price = ''
+        try:
+            status = ad.find('div', class_='products-view-buttons').find('a', class_='btn btn-big btn-buy products-view-buy').text.strip()
+            if status == 'В корзину':
+                status = 'В наличии'
+            else:
+                status = 'Нет в наличии'
+        except:
+            status = 'Нет в наличии'
+        try:
+            url = ad.find('figure', class_='products-view-pictures').find('a', class_='products-view-picture-link products-view-shadow-hover').get('href')
+        except:
+            url = ''
+        race = name
+        data = {'race': race,
+            'title': title,
+            'price': price,
+            'status': status,
+            'url': url}
+        write_csv(data)
+
+def main():
+    url = start_url
+    base_url = base
+
+    total_pages = get_total_pages(get_html(url))
+
+    for i in range(1, total_pages+1):
+        url_gen = base_url + str(i)
+        html = get_html(url_gen)
+        get_page_data(html)
+
+if __name__ == '__main__':
+    main()
